2186d387fde9fbb1f12d5f707fade1fe84393092
Przemekhash <tarapackiprzemek45@gmail.com>
2023-08-30 01:53:47 +0200

    scanning network hosts using nmap

--- GuardianEye/main.cpp
+++ GuardianEye/main.cpp
@@ -59,17 +59,43 @@ void scanPortRange(const std::string& target, int startPort, int endPort, std::v
     }
 }
 
+void scanNetwork(const std::string& subnet, int startPort, int endPort, std::vector<Vulnerability>& openVulnerabilities) {
+    std::string nmapCommand = "nmap -p " + std::to_string(startPort) + "-" + std::to_string(endPort) + " " + subnet;
+    
+    FILE* nmapOutput = popen(nmapCommand.c_str(), "r");
+    if (!nmapOutput) {
+        std::cerr << "Failed to run Nmap command." << std::endl;
+        return;
+    }
+
+    char buffer[128];
+    while (fgets(buffer, sizeof(buffer), nmapOutput) != NULL) {
+        std::string line(buffer);
+        if (line.find("/tcp") != std::string::npos && line.find("open") != std::string::npos) {
+            std::string service;
+            int port = std::stoi(line.substr(0, line.find("/tcp")));
+            if (scanPort(subnet, port, service)) {
+                openVulnerabilities.push_back({port, service, ""});
+            }
+        }
+    }
+
+    pclose(nmapOutput);
+}
+
 int main(int argc, char* argv[]) {
-    if (argc < 4) {
-        std::cerr << "Usage: " << argv[0] << " <target> <startPort> <endPort>" << std::endl;
+    if (argc < 3) {
+        std::cerr << "Usage: " << argv[0] << " <subnet> <startPort> <endPort>" << std::endl;
         return 1;
     }
 
+    std::string subnet = argv[1];
     std::string target = argv[1];
     int startPort = std::stoi(argv[2]);
     int endPort = std::stoi(argv[3]);
 
     std::vector<Vulnerability> openVulnerabilities;
+    scanNetwork(subnet, startPort, endPort, openVulnerabilities);
     std::vector<std::thread> threads;
 
     const int numThreads = std::thread::hardware_concurrency();
