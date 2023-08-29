e2c9fa6825970cbfff7c3423f8f7b7dd5ef2de27
Przemekhash <tarapackiprzemek45@gmail.com>
2023-08-30 01:43:23 +0200

    security verification

--- GuardianEye/main.cpp
+++ GuardianEye/main.cpp
@@ -7,6 +7,7 @@
 
 #include <iostream>
 #include <string>
+#include <sstream>
 #include <vector>
 #include <thread>
 #include <fstream>
@@ -17,7 +18,13 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 
-bool scanPort(const std::string& target, int port) {
+struct Vulnerability {
+    int port;
+    std::string service;
+    std::string vulnerability;
+};
+
+bool scanPort(const std::string& target, int port, std::string& service) {
     int sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock == -1) {
         std::cerr << "Socket error: " << strerror(errno) << std::endl;
@@ -30,6 +37,11 @@ bool scanPort(const std::string& target, int port) {
     server.sin_addr.s_addr = inet_addr(target.c_str());
 
     if (connect(sock, reinterpret_cast<struct sockaddr*>(&server), sizeof(server)) == 0) {
+        char buffer[1024];
+        ssize_t bytesRead = recv(sock, buffer, sizeof(buffer), 0);
+        if (bytesRead > 0) {
+            service = std::string(buffer, bytesRead);
+        }
         close(sock);
         return true;
     } else {
@@ -38,10 +50,11 @@ bool scanPort(const std::string& target, int port) {
     }
 }
 
-void scanPortRange(const std::string& target, int startPort, int endPort, std::vector<int>& openPorts) {
+void scanPortRange(const std::string& target, int startPort, int endPort, std::vector<Vulnerability>& openVulnerabilities) {
     for (int port = startPort; port <= endPort; ++port) {
-        if (scanPort(target, port)) {
-            openPorts.push_back(port);
+        std::string service;
+        if (scanPort(target, port, service)) {
+            openVulnerabilities.push_back({port, service, ""});
         }
     }
 }
@@ -56,7 +69,7 @@ int main(int argc, char* argv[]) {
     int startPort = std::stoi(argv[2]);
     int endPort = std::stoi(argv[3]);
 
-    std::vector<int> openPorts;
+    std::vector<Vulnerability> openVulnerabilities;
     std::vector<std::thread> threads;
 
     const int numThreads = std::thread::hardware_concurrency();
@@ -65,26 +78,56 @@ int main(int argc, char* argv[]) {
     for (int i = 0; i < numThreads; ++i) {
         int threadStartPort = startPort + i * portsPerThread;
         int threadEndPort = (i == numThreads - 1) ? endPort : threadStartPort + portsPerThread - 1;
-        threads.emplace_back(scanPortRange, target, threadStartPort, threadEndPort, std::ref(openPorts));
+        threads.emplace_back(scanPortRange, target, threadStartPort, threadEndPort, std::ref(openVulnerabilities));
     }
 
     for (std::thread& thread : threads) {
         thread.join();
     }
 
-    if (!openPorts.empty()) {
-        std::cout << "Open ports:" << std::endl;
-        for (int port : openPorts) {
-            std::cout << "Port " << port << " is open." << std::endl;
+    // Load vulnerabilities from file
+    std::ifstream vulnerabilitiesFile("vulnerabilities.txt");
+    if (!vulnerabilitiesFile.is_open()) {
+        std::cerr << "Failed to open vulnerabilities file." << std::endl;
+        return 1;
+    }
+
+    std::vector<Vulnerability> knownVulnerabilities;
+    std::string line;
+    while (std::getline(vulnerabilitiesFile, line)) {
+        Vulnerability vulnerability;
+        std::istringstream iss(line);
+        if (!(iss >> vulnerability.port >> vulnerability.service >> vulnerability.vulnerability)) {
+            std::cerr << "Invalid line in vulnerabilities file: " << line << std::endl;
+            continue;
+        }
+        knownVulnerabilities.push_back(vulnerability);
+    }
+    vulnerabilitiesFile.close();
+
+    // Compare open vulnerabilities with known vulnerabilities
+    std::vector<Vulnerability> detectedVulnerabilities;
+    for (const Vulnerability& openVuln : openVulnerabilities) {
+        for (const Vulnerability& knownVuln : knownVulnerabilities) {
+            if (openVuln.port == knownVuln.port && openVuln.service == knownVuln.service) {
+                detectedVulnerabilities.push_back(openVuln);
+            }
+        }
+    }
+
+    if (!detectedVulnerabilities.empty()) {
+        std::cout << "Detected vulnerabilities:" << std::endl;
+        for (const Vulnerability& vuln : detectedVulnerabilities) {
+            std::cout << "Port " << vuln.port << " (" << vuln.service << "): " << vuln.vulnerability << std::endl;
         }
     } else {
-        std::cout << "No open ports found." << std::endl;
+        std::cout << "No known vulnerabilities detected." << std::endl;
     }
 
     std::ofstream outputFile("scan_results.txt");
     if (outputFile.is_open()) {
-        for (int port : openPorts) {
-            outputFile << "Port " << port << " is open." << std::endl;
+        for (const Vulnerability& vuln : detectedVulnerabilities) {
+            outputFile << "Port " << vuln.port << " (" << vuln.service << "): " << vuln.vulnerability << std::endl;
         }
         outputFile.close();
         std::cout << "Scan results saved to scan_results.txt" << std::endl;
