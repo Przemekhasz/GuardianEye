6c3e2af7d2dd80965738fa3a39e3b98f63d0cad6
Przemekhash <tarapackiprzemek45@gmail.com>
2023-08-30 01:06:40 +0200

    Initial Commit

---
+++ GuardianEye/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  GuardianEye
+//
+//  Created by Przemysław Tarapacki on 30/08/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
