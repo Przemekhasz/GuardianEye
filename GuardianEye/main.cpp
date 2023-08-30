f6ebaa937f8818670a1bdef0edc37225e6dfc44a
Przemekhash <tarapackiprzemek45@gmail.com>
2023-08-30 02:01:43 +0200

    refactor scanning network hosts

--- GuardianEye/main.cpp
+++ GuardianEye/main.cpp
@@ -50,15 +50,6 @@ bool scanPort(const std::string& target, int port, std::string& service) {
     }
 }
 
-void scanPortRange(const std::string& target, int startPort, int endPort, std::vector<Vulnerability>& openVulnerabilities) {
-    for (int port = startPort; port <= endPort; ++port) {
-        std::string service;
-        if (scanPort(target, port, service)) {
-            openVulnerabilities.push_back({port, service, ""});
-        }
-    }
-}
-
 void scanNetwork(const std::string& subnet, int startPort, int endPort, std::vector<Vulnerability>& openVulnerabilities) {
     std::string nmapCommand = "nmap -p " + std::to_string(startPort) + "-" + std::to_string(endPort) + " " + subnet;
     
@@ -84,7 +75,7 @@ void scanNetwork(const std::string& subnet, int startPort, int endPort, std::vec
 }
 
 int main(int argc, char* argv[]) {
-    if (argc < 3) {
+    if (argc < 4) {
         std::cerr << "Usage: " << argv[0] << " <subnet> <startPort> <endPort>" << std::endl;
         return 1;
     }
@@ -95,69 +86,14 @@ int main(int argc, char* argv[]) {
 
     std::vector<Vulnerability> openVulnerabilities;
     scanNetwork(subnet, startPort, endPort, openVulnerabilities);
-    std::vector<std::thread> threads;
 
-    const int numThreads = std::thread::hardware_concurrency();
-    const int portsPerThread = (endPort - startPort + 1) / numThreads;
-
-    for (int i = 0; i < numThreads; ++i) {
-        int threadStartPort = startPort + i * portsPerThread;
-        int threadEndPort = (i == numThreads - 1) ? endPort : threadStartPort + portsPerThread - 1;
-        threads.emplace_back(scanPortRange, subnet, threadStartPort, threadEndPort, std::ref(openVulnerabilities));
-    }
-
-    for (std::thread& thread : threads) {
-        thread.join();
-    }
-
-    // Load vulnerabilities from file
-    std::ifstream vulnerabilitiesFile("vulnerabilities.txt");
-    if (!vulnerabilitiesFile.is_open()) {
-        std::cerr << "Failed to open vulnerabilities file." << std::endl;
-        return 1;
-    }
-
-    std::vector<Vulnerability> knownVulnerabilities;
-    std::string line;
-    while (std::getline(vulnerabilitiesFile, line)) {
-        Vulnerability vulnerability;
-        std::istringstream iss(line);
-        if (!(iss >> vulnerability.port >> vulnerability.service >> vulnerability.vulnerability)) {
-            std::cerr << "Invalid line in vulnerabilities file: " << line << std::endl;
-            continue;
-        }
-        knownVulnerabilities.push_back(vulnerability);
-    }
-    vulnerabilitiesFile.close();
-
-    // Compare open vulnerabilities with known vulnerabilities
-    std::vector<Vulnerability> detectedVulnerabilities;
-    for (const Vulnerability& openVuln : openVulnerabilities) {
-        for (const Vulnerability& knownVuln : knownVulnerabilities) {
-            if (openVuln.port == knownVuln.port && openVuln.service == knownVuln.service) {
-                detectedVulnerabilities.push_back(openVuln);
-            }
-        }
-    }
-
-    if (!detectedVulnerabilities.empty()) {
+    if (!openVulnerabilities.empty()) {
         std::cout << "Detected vulnerabilities:" << std::endl;
-        for (const Vulnerability& vuln : detectedVulnerabilities) {
-            std::cout << "Port " << vuln.port << " (" << vuln.service << "): " << vuln.vulnerability << std::endl;
+        for (const Vulnerability& vuln : openVulnerabilities) {
+            std::cout << "Host: " << subnet << " Port: " << vuln.port << " (" << vuln.service << "): " << vuln.vulnerability << std::endl;
         }
     } else {
         std::cout << "No known vulnerabilities detected." << std::endl;
-    }
-
-    std::ofstream outputFile("scan_results.txt");
-    if (outputFile.is_open()) {
-        for (const Vulnerability& vuln : detectedVulnerabilities) {
-            outputFile << "Port " << vuln.port << " (" << vuln.service << "): " << vuln.vulnerability << std::endl;
-        }
-        outputFile.close();
-        std::cout << "Scan results saved to scan_results.txt" << std::endl;
-    } else {
-        std::cerr << "Failed to open output file." << std::endl;
     }
 
     return 0;
